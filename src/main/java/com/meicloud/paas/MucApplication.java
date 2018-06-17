02f02dab0c13d9498310c196ecabce5244212af4
dengb <dengbin19910916@163.com>
2018-06-17 08:14:44 +0800

    Initial commit

---
+++ src/main/java/com/meicloud/paas/MucApplication.java
@@ -0,0 +1,12 @@
+package com.meicloud.paas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MucApplication.class, args);
+    }
+}
